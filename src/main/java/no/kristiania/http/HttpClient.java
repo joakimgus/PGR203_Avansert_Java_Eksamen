83edd55d9278890d50753101a513ea3053662b69
joakimgus <joakim.gus95@gmail.com>
2020-09-27 12:09:31 +0200

    Initial commit

---
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -0,0 +1,85 @@
+package no.kristiania.http;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpClient {
+
+    private int statusCode;
+    private Map<String, String> responseHeaders = new HashMap<>();
+    private String responseBody;
+
+    public HttpClient(final String hostname, int port, final String requestTarget) throws IOException {
+        this(hostname, port, requestTarget, "GET", null);
+    }
+
+    public HttpClient(final String hostname, int port, final String requestTarget, final String httpMethod, String requestBody) throws IOException {
+        Socket socket = new Socket(hostname, port);
+
+        String contentLengthHeader = requestBody != null ? "Content-Length: " + requestBody.length() + "\r\n" : "";
+
+        String request = httpMethod + " " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + hostname + "\r\n" +
+                contentLengthHeader +
+                "\r\n";
+
+        socket.getOutputStream().write(request.getBytes());
+
+        if (requestBody != null) {
+            socket.getOutputStream().write(requestBody.getBytes());
+        }
+
+        String responseLine = readLine(socket);
+        String[] responseLineParts = responseLine.split(" ");
+
+        statusCode = Integer.parseInt(responseLineParts[1]);
+
+        String headerLine;
+        while (!(headerLine = readLine(socket)).isEmpty()) {
+            int colonPos = headerLine.indexOf(':');
+            String headerName = headerLine.substring(0, colonPos);
+            String headerValue = headerLine.substring(colonPos+1).trim();
+
+            responseHeaders.put(headerName, headerValue);
+        }
+
+        int contentLength = Integer.parseInt(getResponseHeader("Content-Length"));
+        StringBuilder body = new StringBuilder();
+        for (int i = 0; i < contentLength; i++) {
+            body.append((char)socket.getInputStream().read());
+        }
+        responseBody = body.toString();
+    }
+
+    public static String readLine(Socket socket) throws IOException {
+        StringBuilder line = new StringBuilder();
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            if (c == '\r') {
+                socket.getInputStream().read(); // read and ignore the following \n
+                break;
+            }
+            line.append((char)c);
+        }
+        return line.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", 80, "/echo?status=404&Content-Type=text%2Fhtml&body=Hello+world");
+        System.out.println(client.getResponseBody());
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getResponseHeader(String headerName) {
+        return responseHeaders.get(headerName);
+    }
+
+    public String getResponseBody() {
+        return responseBody;
+    }
+}
---
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -0,0 +1,97 @@
+package no.kristiania.http;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpServer {
+
+    private File contentRoot;
+    private List<String> memberNames = new ArrayList<>();
+
+    public HttpServer(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    handleRequest(clientSocket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void handleRequest(Socket clientSocket) throws IOException {
+        String requestLine = HttpClient.readLine(clientSocket);
+        System.out.println(requestLine);
+
+        String requestTarget = requestLine.split(" ")[1];
+        String statusCode = "200";
+        String body = "Hello <strong>World</strong>!";
+
+        int questionPos = requestTarget.indexOf('?');
+
+        String requestPath = questionPos != -1 ? requestTarget.substring(0, questionPos) : requestTarget;
+
+        if (questionPos != -1) {
+            QueryString queryString = new QueryString(requestTarget.substring(questionPos+1));
+            if (queryString.getParameter("status") != null) {
+                statusCode = queryString.getParameter("status");
+            }
+            if (queryString.getParameter("body") != null) {
+                body = queryString.getParameter("body");
+            }
+        } else if (!requestPath.equals("/echo")) {
+            File file = new File(contentRoot, requestPath);
+            if (!file.exists()) {
+                body = file + " does not exist";
+                String response = "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: " + body.length() + "\r\n" +
+                        "\r\n" +
+                        body;
+                clientSocket.getOutputStream().write(response.getBytes());
+                return;
+            }
+            statusCode = "200";
+            String contentType = "text/plain";
+            if (file.getName().endsWith(".html")) {
+                contentType = "text/html";
+            }
+            String response = "HTTP/1.1 " + statusCode + " OK\r\n" +
+                    "Content-Length: " + file.length() + "\r\n" +
+                    "Content-Type: " + contentType + "\r\n" +
+                    "\r\n";
+            clientSocket.getOutputStream().write(response.getBytes());
+
+            new FileInputStream(file).transferTo(clientSocket.getOutputStream());
+        }
+
+        String response = "HTTP/1.1 " + statusCode + " OK\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "\r\n" +
+                body;
+
+        clientSocket.getOutputStream().write(response.getBytes());
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = new HttpServer(8080);
+        server.setContentRoot(new File("src/main/resources"));
+    }
+
+    public void setContentRoot(File contentRoot) {
+        this.contentRoot = contentRoot;
+    }
+
+    public List<String> getMemberNames() {
+        return memberNames;
+    }
+}
