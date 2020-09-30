037b885acc55d4c52eb2e2189afc0b3d58def7d6
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-09-30 15:27:13 +0200

    added code from PDF

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -2,68 +2,57 @@ package no.kristiania.http;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
 
 public class HttpClient {
 
-    private final int statusCode;
-    private final Map<String, String> responseHeaders = new HashMap<>();
-    private final String responseBody;
+    private final HttpMessage httpMessage;
+    private int statusCode;
+    private Map<String, String> responseHeaders;
+    String responseBody;
 
+    // Constructor - det som kalles når vi sier new
     public HttpClient(final String hostname, int port, final String requestTarget) throws IOException {
         this(hostname, port, requestTarget, "GET", null);
     }
 
+    // Constructor - det som kalles når vi sier new
     public HttpClient(final String hostname, int port, final String requestTarget, final String httpMethod, String requestBody) throws IOException {
+        // Connect til serven
         Socket socket = new Socket(hostname, port);
 
         String contentLengthHeader = requestBody != null ? "Content-Length: " + requestBody.length() + "\r\n" : "";
 
+        // HTTP Request consists of request line + 0 or more request headers
+        //  request line consists of "verb" (GET, POST, PUT) request target ("/echo", "/echo?status=404"), protocol (HTTP/1.1)
         String request = httpMethod + " " + requestTarget + " HTTP/1.1\r\n" +
+                // request header consists of "name: value"
+                // header host brukes for å angi hostnavnet i URL
                 "Host: " + hostname + "\r\n" +
                 contentLengthHeader +
+                // request ends with empty line
                 "\r\n";
 
+        // send request to server
         socket.getOutputStream().write(request.getBytes());
 
         if (requestBody != null) {
             socket.getOutputStream().write(requestBody.getBytes());
         }
 
-        String responseLine = readLine(socket);
+        httpMessage = new HttpMessage(socket);
+
+        // The first line in the response is called status line or response line
+        // response line consists of protocol ("HTTP/1.1") status code (200, 404, 401, 500) and status message
+        String responseLine = httpMessage.getStartLine();
         String[] responseLineParts = responseLine.split(" ");
 
+        // Status code determines if it went ok (2xx) or not (4xx). (In addition 5xx: server error) 3xx
         statusCode = Integer.parseInt(responseLineParts[1]);
 
-        String headerLine;
-        while (!(headerLine = readLine(socket)).isEmpty()) {
-            int colonPos = headerLine.indexOf(':');
-            String headerName = headerLine.substring(0, colonPos);
-            String headerValue = headerLine.substring(colonPos+1).trim();
+        responseHeaders = httpMessage.getHeaders();
 
-            responseHeaders.put(headerName, headerValue);
-        }
-
-        int contentLength = Integer.parseInt(getResponseHeader("Content-Length"));
-        StringBuilder body = new StringBuilder();
-        for (int i = 0; i < contentLength; i++) {
-            body.append((char)socket.getInputStream().read());
-        }
-        responseBody = body.toString();
-    }
-
-    public static String readLine(Socket socket) throws IOException {
-        StringBuilder line = new StringBuilder();
-        int c;
-        while ((c = socket.getInputStream().read()) != -1) {
-            if (c == '\r') {
-                socket.getInputStream().read(); // read and ignore the following \n
-                break;
-            }
-            line.append((char)c);
-        }
-        return line.toString();
+        responseBody = httpMessage.getBody();
     }
 
     public static void main(String[] args) throws IOException {
