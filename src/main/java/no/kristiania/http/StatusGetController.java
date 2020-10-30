d4b9d13f5c9c0e1f89a6d0c72b1ad3687589b2e9
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-10-30 14:09:05 +0100

    set up StatusGetController

--- src/main/java/no/kristiania/http/StatusGetController.java
+++ src/main/java/no/kristiania/http/StatusGetController.java
@@ -1,5 +1,36 @@
 package no.kristiania.http;
 
+import no.kristiania.database.Status;
+import no.kristiania.database.StatusDao;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URLDecoder;
+import java.sql.SQLException;
+
 public class StatusGetController implements HttpController{
+    private StatusDao statusDao;
+
+    public StatusGetController(StatusDao statusDao) {
+            this.statusDao = statusDao;
+        }
+
+        @Override
+        public void handle(HttpMessage request, Socket clientSocket) throws IOException, SQLException {
+            String body = "<ul>";
+            for (Status status : statusDao.list()) {
+                body += "<li>" + URLDecoder.decode(status.getName(), "utf-8") + "</li>";
+            }
+
+            body += "</ul>";
+            String response = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Length: " + body.getBytes().length + "\r\n" +
+                    "Content-Type: text/html\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n" +
+                    body;
+
+            clientSocket.getOutputStream().write(response.getBytes());
+        }
 
 }
