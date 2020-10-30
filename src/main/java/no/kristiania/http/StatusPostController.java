ed2d3ae177a804df071829c37d81228714f7e246
joakimgus <joakim.gus95@gmail.com>
2020-10-30 14:11:10 +0100

    fixed StatusPostController and UpdateTaskController

--- src/main/java/no/kristiania/http/StatusPostController.java
+++ src/main/java/no/kristiania/http/StatusPostController.java
@@ -1,5 +1,35 @@
 package no.kristiania.http;
 
-public class StatusPostController implements HttpController {
+import no.kristiania.database.Status;
+import no.kristiania.database.StatusDao;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.sql.SQLException;
+
+public class StatusPostController implements HttpController {
+    private final StatusDao statusDao;
+
+    public StatusPostController(StatusDao statusDao) {
+        this.statusDao = statusDao;
+    }
+
+    @Override
+    public void handle(HttpMessage request, Socket clientSocket) throws IOException, SQLException {
+
+        QueryString requestParameter = new QueryString(request.getBody());
+
+        Status status = new Status();
+        status.setName(requestParameter.getParameter("statusName"));
+        statusDao.insert(status);
+
+        String body = "Okay";
+        String response = "HTTP/1.1 200 OK\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n" +
+                body;
+
+        clientSocket.getOutputStream().write(response.getBytes());
+    }
 }
--- src/main/java/no/kristiania/http/UpdateTaskController.java
+++ src/main/java/no/kristiania/http/UpdateTaskController.java
@@ -1,4 +1,41 @@
 package no.kristiania.http;
 
-public class UpdateTaskController {
+import no.kristiania.database.Member;
+import no.kristiania.database.MemberDao;
+import no.kristiania.database.MemberTask;
+import no.kristiania.database.MemberTaskDao;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.sql.SQLException;
+
+public class UpdateTaskController implements HttpController {
+
+    private MemberTaskDao memberTaskDao;
+
+    public UpdateTaskController(MemberTaskDao memberTaskDao) {
+        this.memberTaskDao = memberTaskDao;
+    }
+
+    @Override
+    public void handle(HttpMessage request, Socket clientSocket) throws IOException, SQLException {
+        HttpMessage response = handle(request);
+        response.write(clientSocket);
+    }
+
+    public HttpMessage handle(HttpMessage request) throws SQLException {
+        QueryString requestParameter = new QueryString(request.getBody());
+
+        Integer taskId = Integer.valueOf(requestParameter.getParameter("taskId"));
+        Integer statusId = Integer.valueOf(requestParameter.getParameter("statusId"));
+        MemberTask memberTask = memberTaskDao.retrieve(taskId);
+        memberTask.setStatusId(statusId);
+
+        memberTaskDao.update(memberTask);
+
+        HttpMessage redirect = new HttpMessage();
+        redirect.setStartLine("HTTP/1.1 302 Redirect");
+        redirect.getHeaders().put("Location", "http://localhost:8080/index.html");
+        return redirect;
+    }
 }
