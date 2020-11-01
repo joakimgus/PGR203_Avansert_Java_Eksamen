f48d8d22d7a1ce87d7b13fe69cccadf67a256015
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-11-01 14:46:51 +0100

    might have fixed problem with getting result from inner join into new table

--- src/main/resources/db/migration/V007__create_table_member_task.sql
+++ src/main/resources/db/migration/V007__create_table_member_task.sql
@@ -1,3 +1,6 @@
 CREATE table member_task (
-    member_task_id serial primary key
+    member_task_id serial primary key,
+    member_name varchar,
+    task_title varchar ,
+    task_description varchar
 )
