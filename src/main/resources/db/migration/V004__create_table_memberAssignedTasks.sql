308ea27f269a29ca4bfd617b5fc63bf0f505d25e
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-11-02 19:38:04 +0100

    fixed naming for table

--- src/main/resources/db/migration/V004__create_table_memberAssignedTasks.sql
+++ src/main/resources/db/migration/V004__create_table_memberAssignedTasks.sql
@@ -1,13 +1,13 @@
-create table membersAssignedTasks
+create table members_with_tasks
 (
     member_id integer not null
-        constraint membersAssignedTasks_member_id_fkey
+        constraint members_with_tasks_member_id_fkey
             references members
             on update cascade on delete cascade,
     task_id integer not null
-        constraint membersAssignedTasks_task_id_fkey
+        constraint members_with_tasks_task_id_fkey
             references tasks
             on update cascade on delete cascade,
-    constraint membersAssignedTasks_pk
+    constraint members_with_tasks_pk
         primary key (member_id, task_id)
 );
